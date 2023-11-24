047211e67f2c9c68bad6da5a1c0a8e27f57820a5
elop123 <142408289+elop123@users.noreply.github.com>
2023-11-24 21:27:23 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+var modal = document.getElementById('myModal');
+var openModalBtn = document.getElementById('openModalBtn');
+var closeBtn = document.getElementById('closeBtn');
+
+// Function to open the modal
+function openModal() {
+    modal.style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+ // Add event listener to open modal button
+ openModalBtn.addEventListener('click', openModal);
+
+ // Add event listener to close modal button
+ closeBtn.addEventListener('click', closeModal);
